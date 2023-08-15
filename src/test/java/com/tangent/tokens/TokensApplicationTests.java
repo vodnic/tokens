bfd355dffcb0fb4d255208051eb890b34d4591ae
Przemek <przemek@node>
2023-08-15 14:28:06 +0200

    Fix profile configuration for a test

--- src/test/java/com/tangent/tokens/TokensApplicationTests.java
+++ src/test/java/com/tangent/tokens/TokensApplicationTests.java
@@ -1,8 +1,11 @@
 package com.tangent.tokens;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Collections;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -10,6 +13,7 @@ class TokensApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.setProperty("spring.profiles.active", "test");
 		TokensApplication.main(new String[] {});
 	}
 
